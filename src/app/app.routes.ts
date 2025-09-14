8d06a7d8ef53fbf71ab162dfd3c06264c306dccc
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-09-14 18:35:46 +0630

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Login } from './login/login';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
    {path: 'login', component:Login},
    {path: 'about', component:About},
    {path: 'contact', component:Contact},
    {path: '', component:Home},
+   {path: '**', component:PageNotFound},// use wild card last 
 ];
