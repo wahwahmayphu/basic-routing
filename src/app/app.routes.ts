1194d57b6c4a8ee5a111656964bf1c2ea82f7bee
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-09-14 13:34:23 +0630

    header with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
 export const routes: Routes = [
    {path: 'login', component:Login},
    {path: 'about', component:About},
    {path: 'contact', component:Contact},
+   {path: '', component:Home},
 ];
